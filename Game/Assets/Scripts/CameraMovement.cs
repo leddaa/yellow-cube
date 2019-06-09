d6c16635678d400bf5619b9da217a98860d5dcf3
ledda <robertledang@gmail.com>
2019-06-09 16:45:09 +0200

    Added smooth camera movement

---
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private Transform playerTransform;
+    public Vector3 offset;
+    public float smoothSpeed = 10f;
+
+    private void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        // update camera position
+        Vector3 pos = playerTransform.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, pos, smoothSpeed * Time.deltaTime);
+        Debug.Log(smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPos;
+    }
+
+}

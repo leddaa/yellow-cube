73e37ae1561e2a04840faa65686a281028db5304
ledda <robertledang@gmail.com>
2019-06-17 01:39:57 +0200

    Created main menu

---
+++ Game/Assets/Scripts/Managers/StartManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+}

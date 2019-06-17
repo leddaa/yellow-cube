7d4793f97ef2c4c6b80791c7316ef57c4001cb45
ledda <robertledang@gmail.com>
2019-06-17 22:57:00 +0200

    Added functionality for initializing character

---
+++ Game/Assets/Scripts/Data Types/Character.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Character : MonoBehaviour
+{
+
+    public Mesh mesh;
+    public Material material;
+
+}
---
+++ Game/Assets/Scripts/Player/InitCharacter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InitCharacter : MonoBehaviour
+{
+
+    public Character[] characters;
+
+    private void Start()
+    {
+        int index = PlayerPrefs.GetInt("index");
+        GetComponent<MeshFilter>().mesh = characters[index].mesh;
+        GetComponent<Renderer>().material = characters[index].material;
+    }
+
+}

eedb0574cb4ba2a05e116a67a7f0a184846215bf
ledda <robertledang@gmail.com>
2019-06-18 22:31:02 +0200

    Initial Photon setup

---
+++ Game/Assets/Sandbox/QuickStartLobbyController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    private GameObject startButton;
+    [SerializeField]
+    private GameObject cancelButton;
+    [SerializeField]
+    private int roomSize;
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        startButton.SetActive(true);
+    }
+
+    public void QuickStart()
+    {
+        startButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Quick start");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Join random failed");
+        CreateRoom();
+    }
+
+    private void CreateRoom()
+    {
+
+    }
+
+}
---
+++ Game/Assets/Sandbox/Scripts/NetworkController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to " + PhotonNetwork.CloudRegion);
+    }
+
+    void Update()
+    {
+        
+    }
+}

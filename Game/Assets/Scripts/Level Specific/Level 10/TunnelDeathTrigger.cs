c13e134f80cb58e98ae73d68355218c6c2747dfb
Fudgehog <vlysebo@gmail.com>
2020-08-22 20:56:47 +0200

    Level edits

---
+++ Game/Assets/Scripts/Level Specific/Level 10/TunnelDeathTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TunnelDeathTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    private void OnTriggerStay(Collider player)
+    {
+        if (player.CompareTag("Player"))
+        {
+            var playerSize = player.transform.localScale;
+
+            Debug.Log(playerSize);
+
+            if (playerSize.x != 0.5)
+            {
+                Debug.Log("Kill player");
+                player.GetComponent<PlayerDeath>().Explode();
+                Invoke("RestartLevel", 2f);
+
+
+            }
+        }
+    }
+}
--- Game/Assets/Scripts/Utils/Cheat.cs
+++ Game/Assets/Scripts/Utils/Cheat.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 public class Cheat : MonoBehaviour
 {
     public Material MaterialCheater { get; set; }
-    public float UpwardForce { get; set; } = 1000;
-    public Rigidbody Body { get; set; }
+    public float UpwardForce;
+    public Rigidbody Body;
 
     private TimeTracker timeTracker;
 
@@ -14,15 +14,31 @@ public class Cheat : MonoBehaviour
         timeTracker = GameObject.FindGameObjectWithTag(Tags.TIME_TRACKER).GetComponent<TimeTracker>();
     }
 
+    private void Rotate()
+    {
+        var toggle = this.GetComponent<RandomRotate>().enabled;
+
+        if (toggle == true)
+        {
+            this.GetComponent<RandomRotate>().enabled = false;
+        }
+        else
+        {
+            this.GetComponent<RandomRotate>().enabled = true;
+        }
+    }
+
     private void Update()
     {
         if (Input.GetKey("f"))
         {
             Body.AddForce(0, UpwardForce * Time.deltaTime, 0 );
-            timeTracker.timeSpent += 300 * Time.deltaTime;
-            GameObject.FindGameObjectWithTag(Tags.CHEAT_TEXT).GetComponent<Text>().text = "Cheats Activated!";
-            Renderer rend = GetComponent<Renderer>();
-            rend.material = MaterialCheater;
         }
+
+        if (Input.GetKeyDown("g"))
+        {
+            Rotate();
+        }
+
     }
 }

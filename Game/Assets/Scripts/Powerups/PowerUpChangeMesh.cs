9c6a66834dc0402b751d4568b5e56d7b5850ecb1
Fudgehog <vlysebo@gmail.com>
2020-08-23 03:45:43 +0200

    cleanup and new powerup

---
+++ Game/Assets/Scripts/Powerups/PowerUpChangeMesh.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PowerUpChangeMesh : MonoBehaviour
+{
+    public GameObject pickupEffect;
+    public float ballSize = 1;
+    public float duration = 5;
+    public Mesh mesh;
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag(Tags.AUDIO_MANAGER).GetComponent<AudioManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+    private IEnumerator Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+
+        // Play sound
+        audioManager.Play(Audio.POWERUP_SOUND);
+
+        // Apply effect
+        Mesh initialMesh = player.GetComponent<MeshFilter>().mesh;
+
+        player.GetComponent<MeshFilter>().mesh = mesh;
+        player.GetComponent<MeshCollider>().sharedMesh = mesh;
+
+        player.transform.localScale *= ballSize;
+
+
+        // Disable gameObject
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        // Pause
+        yield return new WaitForSeconds(duration);
+
+        // Reverse effect
+        player.GetComponent<MeshFilter>().mesh = initialMesh;
+        player.GetComponent<MeshCollider>().sharedMesh = initialMesh;
+        player.transform.localScale /= ballSize;
+
+        Destroy(gameObject);
+    }
+}
--- Game/Assets/Scripts/Utils/Audio.cs
+++ Game/Assets/Scripts/Utils/Audio.cs
@@ -1,4 +1,5 @@
 ﻿public class Audio
 {
     public static string COMPLETE_SOUND = "complete_sound";
+    public static string POWERUP_SOUND = "PowerUp_Sound";
 }
